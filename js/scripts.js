dda6ea382edee49fd32332216f1457e67b141700
mrmiguez <r.m.miguez@gmail.com>
2019-04-27 16:50:09 -0700

    cleaning up

--- js/scripts.js
+++ js/scripts.js
@@ -117,28 +117,6 @@ $(document).ready(function () {
         });
     });
     /***************** Smooth Scrolling ******************/
-    /*
-    $(function () {
-
-        $('a[href*=#]')
-              // Remove links that don't actually link to anything
-            .not('[href="#"]')
-            .not('[href="#0"]')
-            .click(function () {
-            if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
-
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                if (target.length) {
-                    $('html,body').animate({
-                        scrollTop: target.offset().top - 90
-                    }, 2000);
-                    return false;
-                }
-            }
-        });
-
-    });*/
     
     // Select all links with hashes
     $('a[href*="#"]')
@@ -165,19 +143,6 @@ $(document).ready(function () {
               .animate({
               scrollTop: target.offset().top - 90
             }, 2000);
-            /*, function() {
-              // Callback after animation
-              // Must change focus!
-              
-              var $target = $(target);
-              $target.focus();
-              if ($target.is(":focus")) { // Checking if the target was focused
-                return false;
-              } else {
-                $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-                $target.focus(); // Set focus again
-              };
-            });*/
           }
         }
       });
